2c92fc2875611a6e4ff5ef8380bf0efb058ac2f1
Jack Mocherman <mochermanjack@gmail.com>
2016-10-01 13:26:50 -0500

    refactored login to return all promises and return matches as well as the user

--- server/controllers/loginCtrl.js
+++ server/controllers/loginCtrl.js
@@ -2,21 +2,53 @@
 
 const User = require('../models/user')
 
-module.exports.logInUser = ({ body : { username, password }}, res, err) => {
+module.exports.logInUser = ({ session, body : { username, password }}, res, err) => {
 	User
 		.findOneByUsername(username)
 		.then((user) => {
 			if(user) {
-				return user.comparePassword(password)
+				return Promise.all([
+					user,
+					user.comparePassword(password)
+				])
 			} else {
 				res.json({ msg: 'User not found'})
 			}
 		})
-		.then(matches => {
+		.then(([user, matches]) => {
 			if(matches) {
+				session.user = user
+				console.log(session)
 				res.json({ user: matches, msg: 'You logged in successfully'})
 			} else {
 				res.json({ msg: 'Password does not match' })
 			}
 		})
 }
+
+// const localStrategy = new Strategy(
+//   {
+//     usernameField: 'username',
+//     passwordField: 'password',
+//   },
+//   (username, password, cb) =>
+//     User.findOneByEmail(username)
+//       .then(user => {
+//         if (user) {
+//           return Promise.all([
+//             user,
+//             user.comparePassword(password),
+//           ])
+//         }
+
+//         cb(null, null, { msg: 'User does not exist in our system' })
+//       })
+//       .then(([user, matches]) => {
+//         if (matches) {
+//           cb(null, user, { msg: 'Successfully logged in' })
+//         } else {
+//           cb(null, null, { msg: 'Password does not match' })
+//         }
+//       })
+//       .catch(cb)
+// )
