0dabf80ae3347008d7646cb6ac9e10b1b9fc5c06
Jack Mocherman <mochermanjack@gmail.com>
2016-10-01 10:17:41 -0500

    log in user function working in login ctrl, still need to add session functionality

--- server/controllers/loginCtrl.js
+++ server/controllers/loginCtrl.js
@@ -2,6 +2,21 @@
 
 const User = require('../models/user')
 
-module.exports.logInUser = (req, res, err) => {
-	console.log(req.body)
+module.exports.logInUser = ({ body : { username, password }}, res, err) => {
+	User
+		.findOneByUsername(username)
+		.then((user) => {
+			if(user) {
+				return user.comparePassword(password)
+			} else {
+				res.json({ msg: 'User not found'})
+			}
+		})
+		.then(matches => {
+			if(matches) {
+				res.json({ user: matches, msg: 'You logged in successfully'})
+			} else {
+				res.json({ msg: 'Password does not match' })
+			}
+		})
 }
