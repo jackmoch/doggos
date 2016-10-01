148b25d7b7f07275e86bcf6b913be7eeac0fb26d
Jack Mocherman <mochermanjack@gmail.com>
2016-10-01 09:11:50 -0500

    checks if user exists in the database and if not, saves username and password in plain text

--- server/controllers/registerCtrl.js
+++ server/controllers/registerCtrl.js
@@ -1,7 +1,22 @@
 'use strict';
 
-// const User = require('../models/user')
+const User = require('../models/user')
 
-module.exports.registerNewUser = (req, res, err) => {
-	console.log(req.body)
+module.exports.registerNewUser = ({ body : { username, password }}, res, err) => {
+	User
+		.findOneByUsername(username)
+			.then(user => {
+				if(user) {
+					res.json({ msg: 'User already exists'})
+				} else {
+					User
+						.create({
+							username: username,
+							password: password
+						})
+						.then((user) => {
+							res.json({ username: username})
+						})
+				}
+			})
 }
