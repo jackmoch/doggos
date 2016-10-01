d41069a46f85934e87a6ecdc76a067d77200f8cc
Jack Mocherman <mochermanjack@gmail.com>
2016-10-01 08:55:32 -0500

    built basic user schema

--- server/models/user.js
+++ server/models/user.js
@@ -1,3 +1,15 @@
 'use strict';
 
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+const userSchema = new mongoose.Schema({
+	username: {
+		type: String,
+		required: true
+	},
+	password: {
+		type: String,
+		required: true
+	}
+})
